0d9a792b8fd1f9ab1aa0369d47ccdb878b3c6b8f
harry-jooyoung-kim <cjsdntlswhz34@gmail.com>
2025-11-06 03:55:11 +0000

    error correction

--- myapp/main.py
+++ myapp/main.py
@@ -1,4 +1,4 @@
-form flask import Flask
+from flask import Flask
 
 app=Flask(__name__)
 @app.route('/')
@@ -6,4 +6,4 @@ def hello_world():
     return 'hello world?'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001)
+    app.run(host='0.0.0.0', port=8001u)
